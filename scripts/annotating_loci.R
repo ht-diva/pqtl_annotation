cd73b0b9646dd7faa709a4f972854393302a7e1e
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-03-11 10:55:07 +0100

    add gene name to locus breaker results #2
    should run it again once receiving missing annotations

---
+++ scripts/annotating_loci.R
@@ -0,0 +1,153 @@
+
+# libraries
+library(tidyverse)
+library(data.table)
+
+#----------#
+
+# inputs (locus_breaker or LB results)
+path_freez <- "/exchange/healthds/pQTL/results/META_CHRIS_INTERVAL/Locus_breaker_cojo_frozen_version_1812024/"
+path_lb_cistrans <- "Archived/mapped_LB_gp_ann_va_ann_bl_ann_collapsed_hf_ann.csv"
+path_lb <- "16-Dec-24_collected_loci_excluding_mhc.csv"
+path_vep <- "/exchange/healthds/pQTL/pQTL_workplace/annotations/VEP/data/snps_ld_in_meta_annot.zip"
+path_vep_extract <- "/exchange/healthds/pQTL/pQTL_workplace/annotations/VEP/data/unzipped/"
+
+# outputs
+path_lb_gene <- paste0(path_freez, "loci_with_genes.tsv")
+
+#----------#
+
+# To add annotated gene name mapping to lead SNP of the locus in LB results:
+#    1. Extract annotated files inside the zip, 
+#    2. List the annotated files
+#    3. Extract columns needed for join with LB
+#    4. Append annotated file name to LB file
+#    5. Write a function to find closest gene name encoding a protein
+#    6. Write a function reading annotated file, filter for lead SNP and find gene name
+#    7. Add annotated gene name by iterating the functions in row-wise manner
+
+#----------#
+# read data files
+lb_cistrans <- data.table::fread(paste0(path_freez, path_lb))
+
+
+# 1. Extract zip (run it once)
+#unzip(path_vep, exdir = path_vep_extract)
+
+
+# 2. List the annotated files in zip
+files_annot <- list.files(paste0(path_vep_extract, "snps_ld_in_meta_annot"), full.names = TRUE)
+
+
+# 3. Extract columns needed for join with LB
+files_split <- files_annot %>%
+  as_tibble() %>%
+  # extract lead SNP, locus and seqid from annotation file name
+  mutate(
+    txtpath = value,  # easy rename; as_tibble used 'value' as the column name
+    txtname = basename(txtpath) %>% str_remove("_snps_.*+"), # get rid of full path, then remove target variant from file name
+    snp     = txtname %>% str_remove_all("_seq.*+") %>% str_replace_all("_", ":"),
+    seqid   = txtname %>% str_remove_all("_chr.*+") %>% str_remove("^\\d+_\\d+_[ATCG]+_[ATCG]+_"),
+    locus   = txtname %>% str_remove_all(".+_chr")
+  ) %>%
+  dplyr::select(seqid, locus, snp, txtpath) %>% # take required column for joining LB file
+  distinct(seqid, locus, .keep_all = T) # in case there is any duplicated annotated file
+
+
+# 4. Append annotated file name to LB file
+lb_annot <- lb_cistrans %>%
+  # rename or reshape ID columns for join
+  dplyr::mutate(
+    seqid = phenotype_id,
+    locus = str_c(chr, start, end, sep = "_")
+  ) %>%
+  left_join(
+    files_split,
+    join_by(seqid, locus)
+  )
+
+
+#----------#
+
+# 5. Function to find name of the closest gene 
+# encoding a protein for each row in LB results
+grep_gene_lb <- function(df) {
+  
+  # Check if any protein coding gene are present
+  if ("protein_coding" %in% df$BIOTYPE) {
+    # Report closest gene by sorting the distance
+    return(
+      df %>%
+        dplyr::filter(BIOTYPE == "protein_coding") %>% # filter out any lincRNA, ncRNAs, etc.
+        top_n(DISTANCE, n = -1) %>% # take the closest gene in vicinity of the lead variant
+        # Collapses target genes into a string
+        summarise(annotated_genes = paste(unique(SYMBOL), collapse = "; ")) %>%
+        pull(annotated_genes)
+    )
+  } else {
+    # Check if any canonical transcripts are present
+    if ("YES" %in% df$CANONICAL) {
+      # Report canonical transcripts
+      return(
+        df %>% 
+          dplyr::filter(CANONICAL == "YES") %>%
+          # Collapses target genes into a string
+          summarise(annotated_genes = paste(unique(SYMBOL), collapse = "; ")) %>% 
+          pull(annotated_genes)
+      ) 
+    }
+    else {
+    # Return empty if no relevant genes found
+    return(data.frame())  
+  }
+  }
+}
+
+
+# 6. Function to read annotated file, subset data 
+# to lead SNP of the locus and find gene name
+take_annot <- function(filepath, variant){
+  data.table::fread(file = filepath) %>% 
+    dplyr::filter(SNPID == variant) %>%
+    grep_gene_lb(.)
+}
+
+#----------#
+
+# 7. iterating the functions
+lb_with_genes <- lb_annot %>%
+  dplyr::filter(!is.na(txtpath)) %>% 
+  #head(10) %>%
+  dplyr::mutate(
+    Gene = map2(txtpath, snp, take_annot) # iterate the function to retrieve combined gene names
+    ) %>%
+  dplyr::select(- txtpath)# %>% View()
+
+
+# save subset of output to Alessia to get her confirmation
+data.table::fwrite(lb_with_genes, file = path_lb_gene, quote = F, row.names = F, sep = "\t")
+
+
+# exit the script in case we run this on clusters
+quit()
+
+
+#----------------------------------------#
+#-----        Test Functions        -----
+#----------------------------------------#
+
+# test above function
+data.table::fread(file = lb_annot$txtpath[157]) %>%
+  dplyr::filter(SNPID == lb_annot$snp[157], BIOTYPE == "protein_coding") %>% 
+  top_n(DISTANCE, n = -10) %>% # take canonical gene if present
+  distinct()
+
+
+# test iteration of the function and externally validate 
+# returned gene name by looking at UCSC genome browser
+map2(
+  lb_annot$txtpath[6971],
+  lb_annot$snp[6971],
+  take_annot
+)
+
