23e3b439bdce61487885db25954751a03afec06f
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-08-05 18:25:46 +0200

    store any genes:consequences pairs with middle/high impact snps #6
    genes_with_consequence_types column in suppl. table 3

--- scripts/annotating_loci_st3.R
+++ scripts/annotating_loci_st3.R
@@ -1,7 +1,8 @@
 
 # libraries
-library(tidyverse)
+library(dplyr)
 library(data.table)
+library(purrr)
 
 #----------#
 
@@ -12,18 +13,16 @@ path_vep_extract <- "/exchange/healthds/pQTL/pQTL_workplace/annotations/VEP/data
 path_cojo <- "16-Dec-24_collected_independent_snps.csv"
 
 # outputs
-path_st3 <- paste0(path_freez, "17-Jul-25_suppl_table_3_cojo_variants_vep_annotated.tsv")
+path_st3 <- paste0(path_freez, "05-Aug-25_suppl_table_3_cojo_variants_vep_annotated.tsv")
 
 #----------#
 
-# To add annotated gene name mapping to lead SNP of the locus in LB results:
-#    1. List annotated files extracted before from zip file sent by Michele F
-#    2. 
-#    3. Extract columns needed for join with LB
-#    4. Append annotated file name to LB file
-#    5. Write a function to find closest gene name encoding a protein
-#    6. Write a function reading annotated file, filter for lead SNP and find gene name
-#    7. Add annotated gene name by iterating the functions in row-wise manner
+# To find genes impacted by middle/high impact COJO or proxy variants:
+#    1. List annotated files extracted before from Michele F's zip file
+#    2. Extract locus to align ST3 with loci list ST2
+#    3. Append path to annotation files to COJO
+#    4. Define a function to find impacted genes and consequences
+#    5. Add annotations by iterating the function row-wise
 
 #----------#
 # read data files
@@ -34,7 +33,7 @@ cojo <- read.csv(paste0(path_freez, path_cojo))
 # 1. List the annotated files in zip
 files_annot <- list.files(paste0(path_vep_extract, "snps_ld_in_meta_annot"), full.names = TRUE)
 
-# 3. Extract columns needed for join with LB
+# 2. Extract locus to align ST3 with loci list ST2
 files_split <- files_annot %>%
   as_tibble() %>%
   # extract COJO SNP, locus, and seqid from filename
@@ -45,93 +44,94 @@ files_split <- files_annot %>%
     seqid   = txtname %>% str_remove_all("^\\d+_\\d+_[ATCG]+_[ATCG]+_") %>% str_remove_all("_(chr\\d+_\\d+_\\d+)|_(\\d+_\\d+_\\d+)"),
     locus   = txtname %>% str_remove_all("^\\d+_\\d+_[ATCG]+_[ATCG]+_(seq.\\d+.\\d+)_") %>% str_remove_all("chr")
   ) %>%
-  dplyr::select(seqid, locus, snp, txtpath) # take required column for joining LB file
+  dplyr::select(seqid, locus, snp, txtpath) # take required column for joining COJO file
 
 
-# 4. Append annotated file name to COJO
+# 3. Append path to annotation files to COJO
 cojo_annot <- cojo %>%
   dplyr::mutate(
     locus = str_remove_all(locus, "chr")
   ) %>%
-  # since there are more than one annotated file for some loci, join by 'SNPID == snp'
-  # ensures that we only take annotation file corresponding to the locus lead variant
-  # among the list of annotation files belonged to all independent snps at a locus
+  # take annotation file corresponding to the locus and COJO variant
   left_join(
     files_split,
     join_by(study_id == seqid, locus, SNP == snp)
   ) %>%
   left_join(
     lb_cistrans %>% mutate(
-      locus = str_c(chr, start, end, sep = "_"), # rename or reshape ID columns for join
-      Ensemble_noisoform = str_remove_all(Ensembl, "\\.\\d+") # remove isoform number from Ensembl_id
+      locus = str_c(chr, start, end, sep = "_") # define locus for join
     ) %>%
-      dplyr::select(phenotype_id, locus, cis_or_trans, Ensemble_noisoform),
+      dplyr::select(phenotype_id, locus, cis_or_trans),
     join_by(study_id == phenotype_id, locus)
   )
 
 
 #----------#
 
-# 5. Function to find annotation of COJO variants
-find_annotation <- function(Ensemble_noisoform, cis_or_trans, cojo_snp, txtpath) {
+# 4. Function to find find impacted genes and consequences
+find_annotation <- function(txtpath) {
   
   # If path is missing or empty or even not exists, returns NA
   # in case, gene id is empty, returns NA
-  if (is.na(txtpath) || txtpath == "" || !file.exists(txtpath) || 
-      (cis_or_trans == "cis" && Ensemble_noisoform == "")) {
-    return(data.table(consequence = NA_character_, affected_gene = NA_character_))
+  if (is.na(txtpath) || txtpath == "" || !file.exists(txtpath)) {
+    return(data.table(txtpath = txtpath, genes_with_consequence_types = NA_character_))
   }
   
   # Read the annotated file
   annot_dt <- data.table::fread(txtpath)
   
-  # Subset to COJO variant
-  cojo_rows <- annot_dt[SNP == cojo_snp]
-  
-  # Default values for consequence (gene-specific check)
-  #consequence <- "No"
-  
-  get_result <- function(rows) {
-    data.frame(
-      consequence   = paste(unique(rows$Consequence), collapse = "; "),
-      affected_gene = paste(unique(rows$SYMBOL), collapse = "; ")
+  # Subset to variants with mid/high impact on any protein-coding genes
+  cojo_rows <- annot_dt %>%
+    dplyr::filter(
+      Consequence %in% epitope_consequences,
+      BIOTYPE == "protein_coding"
     )
+  
+  # If any relevant rows found, collapse consequences per gene
+  if (nrow(cojo_rows) > 0) {
+    
+    combined_impacts <- cojo_rows %>%
+      group_by(SNP, SYMBOL) %>%
+      summarise(
+        vep_annot = paste(unique(Consequence), collapse = ","),
+        .groups = "drop"
+      )
+    
+    # column will be used in Suppl. Table 3
+    genes_with_consequence_types <- combined_impacts %>%
+      mutate(label = paste0(SYMBOL, ": ", vep_annot)) %>%
+      pull(label) %>%
+      unique() %>%
+      paste(collapse = "; ")
+    
+  } else {
+    # otherwise return NA
+    genes_with_consequence_types <- NA_character_
   }
   
-  # Only check Ensembl_id match if "cis"
-  if (cis_or_trans == "cis") {
-    # see if Gene ID matches with one of the ids in Ensembl_id for the seqid
-    gene_ids  <- strsplit(Ensemble_noisoform, ";\\s*")[[1]]
-    gene_rows <- cojo_rows %>% dplyr::filter(Gene %in% gene_ids)
-    
-    return(if (nrow(gene_rows) > 0) get_result(gene_rows) else get_result(cojo_rows))
-
-    # Now COJO variants in trans loci
-    } else {
-      coding_rows    <- cojo_rows[BIOTYPE   == "protein_coding"]
-      canonical_rows <- cojo_rows[CANONICAL == "YES"]
-      
-      if (nrow(coding_rows)    > 0) return(get_result(coding_rows))
-      if (nrow(canonical_rows) > 0) return(get_result(canonical_rows))
-    
-    return(get_result(cojo_rows))
-  }
+  return(
+    data.frame(
+      txtpath,
+      genes_with_consequence_types,
+      stringsAsFactors = FALSE
+    )
+  )
 }
 
 #----------#
 # Supplementary Table 3
 
-# 6. iterate function to read annotated file, subset data to COJO SNP,
-# and find annotation in row-wise manner
-results_annot <- pmap(
-  cojo_annot %>% dplyr::select(Ensemble_noisoform, cis_or_trans, cojo_snp, txtpath),
+# 5. iterate function to read annotated file and find annotation row-wise
+results_annot <- pmap_dfr(
+  cojo_annot %>% dplyr::select(txtpath),
   find_annotation
 )
 
+
 # Combine results with COJO
 st3 <- cojo_annot %>%
-  dplyr::select(- c(txtpath)) %>%
-  cbind(results_annot)
+  right_join(results_annot, join_by(txtpath)) %>%
+  dplyr::select(- txtpath)
 
 #----------#
 # save output
@@ -143,12 +143,4 @@ data.table::fwrite(
   row.names = F
   )
 
-
-lb_annot_missing <- lb_annot %>% 
-  filter(!complete.cases(.)) %>%
-  dplyr::rename(Chr = chr, bp = POS, SNP = SNPID, study_id = phenotype_id) %>%
-  dplyr::select(Chr, bp, SNP, locus, study_id)
-
-
-write.csv(lb_annot_missing, "/scratch/dariush.ghasemi/projects/SNPS_IN_LD/missing_loci.csv", quote = F, row.names = F)
 
